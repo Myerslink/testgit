47393cc64634109a185eb6789b3ade84b8f7f389
Myerslink <73630193+Myerslink@users.noreply.github.com>
2020-10-28 22:33:18 -0700

    Add files via upload
    test upload feature

---
+++ app.py
@@ -0,0 +1,12 @@
+from math import *
+
+char_name = "Jane \\ Doe\""
+char_age = "15"
+print(len(char_name))
+print("TESTING " + char_name + " 2 " + char_age)
+print(sqrt(25))
+print(max(34,12))
+print(min(34,12))
+name = input("Name?")
+age = input("age?")
+print("Hello " + name + " you are " + age)
